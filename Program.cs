a21bdf43cb9144d76ca06ce0868d1292c2bf622b
pedroaro2910@gmail.com <pedroaro2910@gmail.com>
2026-08-13 09:40:09 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string NomeProfissional = "João Programador";
+string EmailContato = "joao.prog@exemplo.com";
+int AnoNascimento = 2007;
+bool EstaDisponivel = true;
+int Idade = DateTime.Now.Year - AnoNascimento;
+
+Console.WriteLine($"Nome do Profissional: {NomeProfissional}");
+Console.WriteLine($"Email de Contato: {EmailContato}");
+Console.WriteLine($"Idade do Contato: {Idade}");
+Console.WriteLine($"Disponibilidade: {(EstaDisponivel ? "Disponível para projeto!" : "Indisponível no momento")}");
+Console.WriteLine();
+
+EstaDisponivel =  false;
+Console.WriteLine($"Nome do Profissional: {NomeProfissional}");
+Console.WriteLine($"Email de Contato: {EmailContato}");
+Console.WriteLine($"Idade do Contato: {Idade}");
+Console.WriteLine($"Disponibilidade: {(EstaDisponivel ? "Disponível para projeto!" : "Indisponível no momento")}");
